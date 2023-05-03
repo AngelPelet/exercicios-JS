bb0bb54db5b2414aba8a3046cc8899257dec119e
AngelPelet <angelpeleteiro2001@gmail.com>
2023-05-03 11:22:05 -0300

    primeiro commit

---
+++ ex008/oi.js
@@ -0,0 +1,7 @@
+var i 
+var soma = 0
+for(i=0; i<=5; i++){
+    soma = soma + 2*i
+}
+
+console.log(soma)
---
+++ ex015/script.js
@@ -0,0 +1,32 @@
+function contar(){
+    let ini = document.getElementById("txti")
+    let fim = document.getElementById("txtf")
+    let passo = document.getElementById("txtp")
+    let res = document.getElementById("res")
+
+    if(ini.value.length == 0 || fim.value.length == 0){
+        alert('[ERRO] Faltam dados!')
+    } else{
+        res.innerHTML = 'Contando: '
+
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0){
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if(i < f){
+            for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c}👉`
+            }
+        } else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c}👉`
+            }
+        }
+        res.innerHTML += `🏁`
+    }
+}
